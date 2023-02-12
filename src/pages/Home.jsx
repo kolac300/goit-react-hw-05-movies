024799b95db39b132d63a9e8d7a6d1c9f1bf29f7
kolac300 <kolac300@gmail.com>
2023-02-12 19:07:06 +0200

    ad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { MoviesList } from 'components/MoviesList'
 import { PopularToday } from 'API'
 import React, { useEffect, useState } from 'react'
-//loader, error 
 const Home = () => {
 	const [movies, setMovies] = useState([])
 	useEffect(() => {
